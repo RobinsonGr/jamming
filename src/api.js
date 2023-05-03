52ec39cf49ebabb66ae937d7be4e0ea06e43b747
Robin <robinsonandresgr@gmail.com>
2023-05-02 22:09:06 -0500

    proxy config to avoid the cors

--- src/api.js
+++ src/api.js
@@ -7,8 +7,6 @@ class API extends React.Component {
     // Initially I thought of using the popstate event Listeneer because in the codeAcademy program  at this point the lifeCycle methods had not yet been seen, much less the useEffect hook. So I finally wanted to go ahead and incorporate the most logical way I had known since before
     componentDidMount() {
 
-        const requestToken = "https://connect.deezer.com/oauth/access_token.php?app_id="
-
         const queryParameters = new URLSearchParams(window.location.search);
 
         if(queryParameters.has("code")) {
@@ -31,28 +29,32 @@ class API extends React.Component {
 
 
      async getToken (authCode) {
+         
+         const API_ID = 601224;
+         const secretKey = "65b477d68d855adee8b009895c18c152";
+         const code = authCode;
 
-        const API_ID = 601224;
-        const secretKey = "65b477d68d855adee8b009895c18c152";
-        const code = authCode;
+         const queryParams = new URLSearchParams({
+            app_id: API_ID,
+            secret: secretKey,
+            code: code,
+          });
+         
+         const tokenResponse = await fetch(`http://localhost:5000/get-token?${queryParams.toString()}`, {
+             method: "POST",                
+            })
 
-        const tokenResponse = await fetch("https://cors-anywhere.herokuapp.com/https://connect.deezer.com/oauth/access_token.php?", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/x-www-form-urlencoded"
-        },
-        body: `app_id=${API_ID}&secret=${secretKey}&code=${code}`
+        //const token = await tokenResponse.text();
+        const token2 = await tokenResponse.json();
 
-        })
-        const token = await tokenResponse.json()
-
-
-        console.log("token:" + token)
+        //window.location.href = "/"
+       
+        console.log(token2.expires);
      }
 
-        
+     
+     
 
-    
     render () {
         return (
             <button onClick={this.redirectToDeezer}> ¡Let's Start!</button>
